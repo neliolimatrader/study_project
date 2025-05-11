6595e97f0114d49280ef36bb49ab5d709ba66af9
Nelio Amorim <neliolima.trader@gmail.com>
2025-05-11 18:09:12 -0300

    reload

--- Projeto01/app.js
+++ Projeto01/app.js
@@ -1,16 +1,16 @@
 alert('Boas vindas ao jogo do número secreto');
-let numerosecreto = 5;
-console.log(numerosecreto)
+let numeroMaximo = 5000;
+let numerosecreto = parseInt(Math.random() * numeroMaximo +1);
+console.log(numerosecreto);
 let chute;
 let tentativas = 1;
 
 // se chute for igual ao numero secreto 
 while (chute != numerosecreto) {
-    chute = prompt('Escolha um numero entre 1 e 10');
+    chute = prompt(`Escolha um numero entre 1 a ${numeroMaximo}`);
     // se chute for igual ao numero secreto
     if (chute == numerosecreto) {
-        alert(`Isso ai, você descobriu o numero secreto ${numerosecreto} com ${tentativas} tentativas`);
-
+        break;
     } else {
         if (chute > numerosecreto) {
             alert(`O numero secreto é menor que ${chute}`)
@@ -23,4 +23,5 @@ while (chute != numerosecreto) {
     }
 
 }
-
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai, você descobriu o numero secreto ${numerosecreto} com ${tentativas} ${palavraTentativa} `);
